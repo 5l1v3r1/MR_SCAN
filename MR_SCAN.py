b82ba89db4c756077883b1e8f92688225dd6ff55
B3rt1ng <43414019+b3rt1ng@users.noreply.github.com>
2019-04-20 02:26:20 +0200

    Update MR_SCAN.py

--- MR_SCAN.py
+++ MR_SCAN.py
@@ -1,5 +1,4 @@
 import sys, os, subprocess
-from scapy.all import *
 BLUE, RED, WHITE, YELLOW, MAGENTA, GREEN, END = '\33[94m', '\033[91m', '\33[97m', '\33[93m', '\033[1;35m', '\033[1;32m', '\033[0m'
 full_ip = ''
 dot = '.'
